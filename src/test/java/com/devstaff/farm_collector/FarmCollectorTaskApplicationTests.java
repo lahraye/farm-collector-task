5b9c6b1556ddc1386d272de17e0719cab7da73ca
olarewaju.showemimo <olarewaju.showemimo@teamapt.com>
2024-10-05 16:22:57 +0100

    inital commit

---
+++ src/test/java/com/devstaff/farm_collector/FarmCollectorTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstaff.farm_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmCollectorTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
